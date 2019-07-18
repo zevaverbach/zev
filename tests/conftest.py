b9318067207f549103a4a4b19aeda3f4c0e74ad9
zevav <zev@averba.ch>
2019-07-18 13:37:52 +0200

    making tests work

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,16 @@
+import os
+
 from pytest import fixture
 
 
 @fixture
 def empty_filepath():
-    return '/tmp/empty.txt'
+    """didn't use /tmp because it makes Travis sad"""
+    return 'empty.txt'
 
 
 @fixture
 def empty_file(empty_filepath):
     open(empty_filepath, 'w').close()
-
-
+    # this is instead of yield statement to make this work with Python 2
+    request.addfinalizer(lambda _: os.remove(empty_filepath))
